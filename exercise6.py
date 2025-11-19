fc47858be5ae17c1b954aae22f3ae19ba20a8937
Ritu Dhanani <ritu.taglineinfo.in>
2025-11-19 16:35:22 +0530

    complete

---
+++ exercise6.py
@@ -0,0 +1,12 @@
+string="PQRQRQRQ"
+substring="QRQ"
+
+count=0
+start=0
+while True:
+    start=string.find(substring,start)
+    if start==-1:
+        break
+    count+=1
+    start+=1
+print("The count of overlapping occurrences of substring is =",count)
