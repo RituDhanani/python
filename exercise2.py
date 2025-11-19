ad2d14cdbcbe631c406a254c25459b3ebdeeec55
Ritu Dhanani <ritu.taglineinfo.in>
2025-11-19 11:38:17 +0530

    complete

--- exercise2.py
+++ exercise2.py
@@ -1,8 +1,29 @@
 names=["smith","johnson","jones","brown","davis","miller","wilson","moore","taylor","anderson","thoms","jackson","white"]
+from collections import defaultdict
 
 print(names)
-length=["\n"+str(len(nam)) for nam in names]
+
+length=[(len(nam)) for nam in names]
 print("length of each name is ",length)
+
+groups= defaultdict(list)
+for nam in names:
+    groups[len(nam)].append(nam)
+
+most_freq=sorted(groups.items(), key=lambda x: len(x[1]), reverse=True)
+print("three most frequent name length are:")
+for length, group in most_freq[:3]:
+    print(f"{len(group)} names of length {length}: {group}")
+
+
+least_freq=sorted(groups.items(), key=lambda x: len(x[1]))
+print("three least frequent name length are:")
+for length, group in least_freq[:3]:
+    print(f"{len(group)} names of length {length}: {group}")
+
+
+
+
 
 
 
