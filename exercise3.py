2d055caf5e7d80af932b27a855ad256d5e56ffaf
Ritu Dhanani <ritu.taglineinfo.in>
2025-11-19 12:38:19 +0530

    complete

--- exercise3.py
+++ exercise3.py
@@ -1,3 +1,14 @@
-sentence=["my name is ram","he is a good person","you should be careful while coding","he can do better","the person is mysterious","jay shree ram","it is my pen"]
+sentences=["my name is ram","he is a good person","you should be careful while coding","he can do better","the person is mysterious","jay shree ram","it is my pen"]
 
-print(sentence)
+word_trees=[sentance.split()for sentance in sentences]
+print("list of word trees is ",word_trees)
+
+count={}
+for tree in word_trees:
+    for word in tree:
+        if word in count:
+            count[word]+=1
+        else:
+            count[word]=1
+
+print("\n number of word count is ",count)
