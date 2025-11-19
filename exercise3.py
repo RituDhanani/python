9cb47c8d6d699a939ebb406e38660fd7bbf166c7
Ritu Dhanani <ritu.taglineinfo.in>
2025-11-19 12:06:56 +0530

    Add print statement

--- exercise3.py
+++ exercise3.py
@@ -1,2 +1,3 @@
 sentence=["my name is ram","he is a good person","you should be careful while coding","he can do better","the person is mysterious","jay shree ram","it is my pen"]
 
+print(sentence)
