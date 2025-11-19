d51f0ba56a1065b4204eaae4a07713424a35289c
Ritu Dhanani <ritu.taglineinfo.in>
2025-11-19 14:53:45 +0530

    complete

--- exercise4.py
+++ exercise4.py
@@ -11,3 +11,13 @@ print("Fibonacci 8  =", fib(8))
 print("Fibonacci 60 =", fib(60))   
 print("Fibonacci 90 =", fib(90))  
 
+
+number=[22,44,5,67,1,1,2,4,5]
+def sum(number):
+    if len(number)==0:
+        return 0
+    else:
+        return number[0]+sum(number[1:])
+print("Sum of given list number is  =",sum(number))
+
+    
