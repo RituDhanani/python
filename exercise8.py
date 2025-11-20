06832c43792e3eeabbb6102453c2fb255613f380
Ritu Dhanani <ritu.taglineinfo.in>
2025-11-20 10:13:04 +0530

    complete

---
+++ exercise8.py
@@ -0,0 +1,11 @@
+numbers=[2,5,6,1,3,4,7,8,9,10]
+n=len(numbers)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if numbers[j]>numbers[j+1]:
+            numbers[j],numbers[j+1]=numbers[j+1],numbers[j]
+       
+       
+print(numbers)
+
+
